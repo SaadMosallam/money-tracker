59c96e8b3e9ee49cc1cf73292b0ee0fe04182883
Saad <saad.mosallam@4sale.tech>
2026-01-23 20:19:35 +0200

    add shadcn integration and integrate some basic components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,20 @@
-import { getBalances } from '@/lib/db/queries/balances';
+import { getBalanceDataset } from "@/lib/db/queries/balances";
+import { calculateBalances } from "@/lib/calculations/calculateBalances";
 
-export const revalidate = 0;
+import { PageContainer } from "@/lib/ui/components/layout/PageContainer";
+import { BalanceTable } from "@/lib/ui/components/balance/BalanceTable";
+import { PairwiseDebts } from "@/lib/ui/components/balance/PairwiseDebts";
 
-const page = async () => {
-    const balances = await getBalances();
-  
-    return (
-    <div>
-        <h1>Balances</h1>
-        <pre>{JSON.stringify(balances, null, 2)}</pre>
-    </div>
-  )
+export default async function DashboardPage() {
+  const dataset = await getBalanceDataset();
+  const balances = calculateBalances(dataset);
+
+  return (
+    <PageContainer title="Dashboard">
+      <div className="space-y-8">
+        <BalanceTable balances={balances} />
+        <PairwiseDebts balances={balances} />
+      </div>
+    </PageContainer>
+  );
 }
-
-export default page
